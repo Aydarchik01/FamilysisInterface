cfd0feeb7ae521d11f985190b397888e8bd0c670
kuba <Kudaiberdigapyrov@gmail.com>
2023-04-04 19:41:20 +0600

    beka commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("hjkl;");
         }
     }
-
-        System.out.println("-_- я не виновата");
-    }
-}
